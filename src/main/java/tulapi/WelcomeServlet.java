efbd792acbb0a689789d06f8e2534195ba694a4e
Stephan Zlatarev <stephan.zlatarev@sap.com>
2014-04-13 08:49:24 +0300

    Initial commit

---
+++ src/main/java/tulapi/WelcomeServlet.java
@@ -0,0 +1,18 @@
+package tulapi;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class WelcomeServlet extends HttpServlet {
+
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+      response.setContentType("text/plain");
+      response.getWriter().println("Welcome, kids!");
+  }
+
+}

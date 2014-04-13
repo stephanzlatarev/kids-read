e0d806d31bfef248df468ce7c8a1d22a2f92a293
stephanzlatarev <stephan.zlatarev@gmail.com>
2014-04-13 12:18:33 +0300

    Update WelcomeServlet.java

--- src/main/java/tulapi/WelcomeServlet.java
+++ src/main/java/tulapi/WelcomeServlet.java
@@ -12,7 +12,7 @@ public class WelcomeServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
       response.setContentType("text/plain");
-      response.getWriter().println("Welcome, kids!");
+      response.getWriter().println("Welcome, kids! version 1.1");
   }
 
 }
